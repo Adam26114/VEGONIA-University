d7db0a11d0771b0380c58a56f6d390ae6e75c709
Adam26114 <zakato26114@gmail.com>
2022-08-25 09:40:21 +0630

    24 Aug 2022

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
-$(document).ready(function(){
+// Start Jquery Area
 
-    // Strat Navbar
-    $('.navbuttons').click(function(){
-        $('.navbuttons').toggleClass('crossxs');
-    });
-    // End Navbar
+$(document).ready(function(){
+    
+    // Start Header
+    
+        // Strat Navbar
+            $('.navbuttons').click(function(){
+                $(this).toggleClass('crossxs');
+            });
+
+        // End Navbar
+    // End Header
 });
+
+// End Jquery Area
+
+// Start Javascript Area
+
+
+
+// End Javascript Area
