4f29e558dcfd298606142d770f485242c9925710
Adam26114 <zakato26114@gmail.com>
2022-08-25 11:08:53 +0630

    25 Aug 2022

--- js/app.js
+++ js/app.js
@@ -30,9 +30,33 @@ $(document).ready(function(){
         getcountervalue.textContent = 0;
         // console.log(getcountervalue);
 
+        const updatecounter = function(){
+            // console.log("i am working");
+
+            const getcttarget = +getcountervalue.getAttribute('data-target')
+
+            // console.log(getcttarget);
+            // console.log(typeof getcttarget,getcttarget);
+
+            const getctcontent = +getcountervalue.innerText
+            // console.log(typeof getctcontent,getctcontent);
+
+            const incnumber = getcttarget / 100;
+            // console.log(incnumber);
+
+            if(getctcontent < getcttarget){
+                getcountervalue.textContent = getctcontent + incnumber;
+
+                setTimeout(updatecounter,30);
+            }
+        }
+
+        updatecounter();
 
     })
 
     // End Student Counter Section
 
 // End Javascript Area
+
+// 25CT
