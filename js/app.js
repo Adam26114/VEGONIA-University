1d339c38daf738365f5915211ff54f1cf247e86c
Adam26114 <86336270+Adam26114@users.noreply.github.com>
2022-09-02 09:07:37 +0630

    2 Aug 2022

--- js/app.js
+++ js/app.js
@@ -1,61 +1,57 @@
 // Start Jquery Area
 
-$(document).ready(function(){
-    
-    // Start Header
-    
-        // Strat Navbar
-            $('.navbuttons').click(function(){
-                $(this).toggleClass('crossxs');
-            });
+$(document).ready(function () {
+  // Start Header
 
-        // End Navbar
-    // End Header
+  // Strat Navbar
+  $(".navbuttons").click(function () {
+    $(this).toggleClass("crossxs");
+  });
+
+  // End Navbar
+  // End Header
 });
 
 // End Jquery Area
 
-
 // -------------------------------------------------------------
-
 
 // Start Javascript Area
 
-    // Start Student Counter Section
+// Start Student Counter Section
 
-    var getcountervalues = document.querySelectorAll('.countervalues')
-    // console.log(getcountervalues);
+var getcountervalues = document.querySelectorAll(".countervalues");
+// console.log(getcountervalues);
 
-    getcountervalues.forEach(function(getcountervalue){
-        getcountervalue.textContent = 0;
-        // console.log(getcountervalue);
+getcountervalues.forEach(function (getcountervalue) {
+  getcountervalue.textContent = 0;
+  // console.log(getcountervalue);
 
-        const updatecounter = function(){
-            // console.log("i am working");
+  const updatecounter = function () {
+    // console.log("i am working");
 
-            const getcttarget = +getcountervalue.getAttribute('data-target')
+    const getcttarget = +getcountervalue.getAttribute("data-target");
 
-            // console.log(getcttarget);
-            // console.log(typeof getcttarget,getcttarget);
+    // console.log(getcttarget);
+    // console.log(typeof getcttarget,getcttarget);
 
-            const getctcontent = +getcountervalue.innerText
-            // console.log(typeof getctcontent,getctcontent);
+    const getctcontent = +getcountervalue.innerText;
+    // console.log(typeof getctcontent,getctcontent);
 
-            const incnumber = getcttarget / 100;
-            // console.log(incnumber);
+    const incnumber = getcttarget / 100;
+    // console.log(incnumber);
 
-            if(getctcontent < getcttarget){
-                getcountervalue.textContent = getctcontent + incnumber;
+    if (getctcontent < getcttarget) {
+      getcountervalue.textContent = getctcontent + incnumber;
 
-                setTimeout(updatecounter,30);
-            }
-        }
+      setTimeout(updatecounter, 30);
+    }
+  };
 
-        updatecounter();
+  updatecounter();
+});
 
-    })
-
-    // End Student Counter Section
+// End Student Counter Section
 
 // End Javascript Area
 
